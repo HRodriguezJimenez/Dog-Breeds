151c144e673a9fcabdb9f71efe4ad356a872b655
HRodriguezJimenez <harol469@gmail.com>
2023-12-23 16:53:46 -0500

    formOk

--- PI-Dogs-main/client/src/redux/utilsRedux/filterAndSortFunctions.js
+++ PI-Dogs-main/client/src/redux/utilsRedux/filterAndSortFunctions.js
@@ -1,15 +1,13 @@
 // Esta función nos retorna un array con los perros que tengan el temperamento seleccionado para realizar el filtro.
 export function filterByTemperament(dogs, temperaments) {
   let temperamentDogs = [];
-  if (temperaments === "all") {
-    temperamentDogs = dogs;
-  } else {
-    temperamentDogs = dogs?.filter((dog) => {
-    return  dog.created || (dog.Temperaments && dog.Temperaments.length > 0 && dog.Temperaments.some((temp) => temperaments.includes(temp)));
-    });
-  }
-  
-  return temperamentDogs;     
+  temperaments === "all"
+    ? (temperamentDogs = dogs)
+    : (temperamentDogs = dogs?.filter((dog) =>
+        dog.Temperaments?.some((temp) => temperaments.includes(temp))
+      ));
+
+  return temperamentDogs;
 }
 
 // En esta función realizamos un filtro que nos muestre los dogs basandose en la propiedad created.
@@ -65,7 +63,7 @@ export const orderByWeight = (dogs, order) => {
 
 // En esta función incorporamos las funciones anteriores y es la  que vamos a usar para enviar la información filtrada y ordenada, recibe como parametros la lista de dogs para filtrar y un objeto de nombre configs que trae las propiedades específicas para realizar el filtrado y ordenamiento.
 export default function dogsSortedAndFiltered(dogs, configs = {}) {
-  const { temperament = {}, origin = {}, order = {}} = configs;
+  const { temperament = {}, origin = {}, order = {} } = configs;
 
   let filteredAndOrdered = [ ...dogs ]; // Creamos una copia del estado original.
 
